e1a57fba19f11fec33d3a414f150be6716f5ec20
sat1ss <91979391+sat1ss@users.noreply.github.com>
2022-11-17 23:00:46 +0100

    Added printer logic
    update v2

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
-import os
+import os, win32print
 from shutil import move
 from time import sleep
 from datetime import date
 
 Folder_to_print = r"C:\Printer"
 Current_date_folder = f"{Folder_to_print}\{date.today().strftime('%d.%m.%Y')}"
+        
+def Default_printer():        
+        printer = win32print.GetDefaultPrinter()
+        return printer
 
 # Creating folder (DD.MM.YYYY)
 try:
@@ -12,6 +16,9 @@ try:
 except Exception:
         print(f"{Current_date_folder} already exist")
 
+#Changing default printer to physical printer
+if Default_printer() != "Brother DCP-J105 Printer":
+        win32print.SetDefaultPrinter("Brother DCP-J105 Printer")
 
 # Loop to search for files to print, doing it and then moving them to correct directory
 for file in os.listdir(Folder_to_print):
@@ -23,3 +30,7 @@ for file in os.listdir(Folder_to_print):
                 os.startfile(file_path, "print")        
                 sleep(8)
                 move(file_path, Current_date_folder)
+
+if Default_printer() != "Microsoft Print to PDF":
+        win32print.SetDefaultPrinter("Microsoft Print to PDF")
+
