1fa438c1d25ea5a0554d5e59a4ed673da3692cd4
sat1ss <91979391+sat1ss@users.noreply.github.com>
2022-11-17 23:13:22 +0100

    More readable comments are added

--- Main.py
+++ Main.py
@@ -5,12 +5,13 @@ from datetime import date
 
 Folder_to_print = r"C:\Printer"
 Current_date_folder = f"{Folder_to_print}\{date.today().strftime('%d.%m.%Y')}"
-        
+
+#This function read default printer, and return it
 def Default_printer():        
         printer = win32print.GetDefaultPrinter()
         return printer
 
-# Creating folder (DD.MM.YYYY)
+# Creating folder (DD.MM.YYYY), if doesn't exist
 try:
         os.mkdir(Current_date_folder)
 except Exception:
@@ -20,17 +21,18 @@ except Exception:
 if Default_printer() != "Brother DCP-J105 Printer":
         win32print.SetDefaultPrinter("Brother DCP-J105 Printer")
 
-# Loop to search for files to print, doing it and then moving them to correct directory
+#Loop that searches for files to print, does that, then move them to the appropriate directory
 for file in os.listdir(Folder_to_print):
 
         file_path = os.path.join(Folder_to_print, file)
 
         if os.path.isfile(file_path):
                 print(f"Printing {file}")
-                os.startfile(file_path, "print")        
+                os.startfile(file_path, "print")
                 sleep(8)
                 move(file_path, Current_date_folder)
 
+#Changing default printer to PDF 
 if Default_printer() != "Microsoft Print to PDF":
         win32print.SetDefaultPrinter("Microsoft Print to PDF")
 
