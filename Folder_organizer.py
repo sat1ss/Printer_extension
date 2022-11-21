6424cabd01c4c7a3f6db1a8a87c572cbf31f80ea
sat1ss <91979391+sat1ss@users.noreply.github.com>
2022-11-21 23:32:26 +0100

    Update Folder_organizer.py
    I'm considering getting last modified date instead of doing it by name

--- Folder_organizer.py
+++ Folder_organizer.py
@@ -1,8 +1,32 @@
+import os 
+from shutil import rmtree
 from datetime import date, timedelta
+from time import sleep
 
 today = date.today().strftime('%d.%m.%Y')
 last_seven_days = []
+Folder_to_clear = r"C:\Printer"
+counter = 0
 
-
+# Looking for last seven days, save it into list
 for x in range(7):
-    day = date.today() - timedelta(days=x)
+    day = str(date.today() - timedelta(days=x))
+    day = f"{day[-2:]}.{day[-5:-3]}.{day[0:4]}"
+    last_seven_days += [day]
+
+# Loop to search for files in chosen folder
+for object in os.listdir(Folder_to_clear):
+
+    path = os.path.join(Folder_to_clear, object)
+    
+    # If chosen object is not in last seven day list, and object is folder, then it will be automaticlly deleted
+    if os.path.isdir(path):
+        if object not in last_seven_days:
+            print(f"Deleting {object} ...")
+            print('If you wanna cancel this process press "Ctrl + C" ')
+            sleep(5)
+            rmtree(path)
+            # Counter added for safety reasons
+            counter += 1
+            if counter >= 3:
+                exit()
